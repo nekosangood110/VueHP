70e07442164f6886ea8e89ca039239e7a262a4ea
nekosangood110 <asuha2314@gmail.com>
2020-10-06 14:21:41 +0900

    コミット

---
+++ src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="contact">
+    
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TabMenu.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="(item, index) in items" :key="index">
+        <roter-link v-bind:to="item.path">
+          {{ item.title }}
+        </roter-link>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TavMenu',
+  data () {
+    return {
+      items: [
+        { title: 'top', path: '/' },
+        { title: 'profile', path: '/profile' },
+        { title: 'contact', path: '/contact' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+ol {
+  
+  display: flex;
+  justify-content: center;
+  padding: 0px;
+  list-style: none;
+}
+
+li {
+  padding: 20px;
+  text-align: center;
+  margin: 0 30px;
+  width: 480px;
+  font-size: 25px;
+  /* font-family: Arial, Helvetica, sans-serif; */
+  border-radius: 9px;
+  color: #ffffff;
+  background-color: #99aabf;
+}
+li:hover {
+  opacity: 0.8;
+}
+/* router-linkの部分 */
+/* a {
+  color: #757575;
+} */
+</style>
