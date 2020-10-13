92ac6a7704ac6d6d4281d604ff9de66acb5ab854
LAPTOP-SAHND00I\asuha <asuha2314@gmail.com>
2020-10-13 22:06:09 +0900

    コミット

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,4 @@
 <template>
-<!-- topのページここにかいてる -->
 <div class="Contact">
     Contact
 </div>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,24 @@
 <template>
-<div>a</div>
+<div class="contents">
+    <div class="profile">
+        <img src="" alt="">
+        <h2>About</h2>
+        <div class="my-profile">
+        <h3>profile</h3>
+        <ul>
+            <li><span class="">Name:</span> Chikako Ono</li>
+            <li><span class="">Birthplace:</span> Nara</li>
+            <li><span class="">Birth:</span> 2001-03-14</li>
+            <li><span class="">Hobby:</span> Cafe-hopping, Ride a bike</li>
+
+        </ul>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-// export default {
-//     name: "",
-// }
+
 </script>
 
 <style scoped>
